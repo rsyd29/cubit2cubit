5d41ef0eabeec60431ffeee4a85454175dc470ec
rsyd29 <brasyid15@gmail.com>
2022-07-30 21:37:12 +0700

    Create BlocProvider for ColorCubit and CounterCubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:cubit2cubit/cubits/color/color_cubit.dart';
+import 'package:cubit2cubit/cubits/counter/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +12,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider<ColorCubit>(
+      create: (context) => ColorCubit(),
+      child: BlocProvider<CounterCubit>(
+        create: (context) => CounterCubit(
+          colorCubit: context.read<ColorCubit>(),
+        ),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MyHomePage(),
+        ),
       ),
-      home: const MyHomePage(),
     );
   }
 }
